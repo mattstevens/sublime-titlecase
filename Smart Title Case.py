de3626cfd629b10b35bd7b182420458a451fc4fb
Matt Stevens <matt@alloysoft.com>
2013-01-31 22:15:52 -0700

    Convert to spaces

--- Smart Title Case.py
+++ Smart Title Case.py
@@ -2,11 +2,11 @@ import sublime, sublime_plugin
 from titlecase import titlecase
 
 class SmartTitleCaseCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		for region in self.view.sel():
-			if region.empty():
-				region = self.view.line(region)
+    def run(self, edit):
+        for region in self.view.sel():
+            if region.empty():
+                region = self.view.line(region)
 
-			s = self.view.substr(region)
-			s = titlecase(s)
-			self.view.replace(edit, region, s)
+            s = self.view.substr(region)
+            s = titlecase(s)
+            self.view.replace(edit, region, s)
